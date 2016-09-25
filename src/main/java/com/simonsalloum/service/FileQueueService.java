ac45e6ac00ed8ac0ed2718fccd5325c896eace0b
Simon Salloum <simon.salloum@digitalroute.com>
2016-09-25 22:10:34 +0200

    hardcode file path for now

--- src/main/java/com/simonsalloum/service/FileQueueService.java
+++ src/main/java/com/simonsalloum/service/FileQueueService.java
@@ -2,29 +2,32 @@ package com.simonsalloum.service;
 
 public class FileQueueService implements QueueService<QueueServiceRecord, QueueServiceResponse> {
 
-    private final String path;
+    private static String path;
+    private static String fileName;
 
-    public FileQueueService(String path) {
-        this.path = path;
+    public FileQueueService(String filePath) {
+        path = filePath;
     }
 
     // TODO: inject path into constructor from config and remove this constructor
     public FileQueueService() {
-        this.path = "/Users.simon.salloum/Desktop";
+        path = "/Users.simon.salloum/Desktop";
     }
 
+    // TODO: start out with synchronization, see if it can be done better later
+
     @Override
-    public QueueServiceResponse push(QueueServiceRecord record) {
+    public synchronized QueueServiceResponse push(QueueServiceRecord record) {
         return new QueueServiceResponse(QueueServiceResponseCode.RECORD_PRODUCED, record);
     }
 
     @Override
-    public QueueServiceResponse pull() {
+    public synchronized QueueServiceResponse pull() {
         return null;
     }
 
     @Override
-    public void delete(QueueServiceRecord record) {
+    public synchronized void delete(QueueServiceRecord record) {
 
     }
 }
